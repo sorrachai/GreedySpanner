01bd442c564c422105ef2a44fc11fe80827d4316
Sorrachai Yingchareonthawornchai <sorrachai.cp@gmail.com>
2025-09-06 15:43:16 +0200

    update

--- GreedySpanner.lean
+++ GreedySpanner.lean
@@ -1,3 +1,3 @@
 -- This module serves as the root of the `GreedySpanner` library.
 -- Import modules here that should be built as part of the library.
-import «GreedySpanner».Basic
+import «GreedySpanner».GreedySpanner
